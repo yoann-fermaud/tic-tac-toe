c6d34f37818edeae97320ec29ffc68c693b9bcd7
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-01-11 10:07:58 +0100

    tic-tac-toe game patch (add game mode)

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from py_ai.ia import ia
 
-
-instructions = """
-This is the tic tac toe board
-
-   1 ┃ 2 ┃ 3         1 ║ 2 ║ 3
- ━━━━╋━━━╋━━━━      ═══╬═══╬═══
-   4 ┃ 5 ┃ 6         4 ║ 5 ║ 6
- ━━━━╋━━━╋━━━━      ═══╬═══╬═══
-   7 ┃ 8 ┃ 9         7 ║ 8 ║ 9
-
-*intstruction :
-1. Insert the spot number (1-9) to put your sign
-2. You must fill all 9 spots to get the result
-3. Player 1 will go first 
-"""
+print("""
+ _____  _  ____         _____  _____  ____         _____  ____  _____
+/__ __\/ \/   _\       /__ __\/  _  \/   _\       /__ __\/  _ \/  __/    
+  / \  | ||  /   _____   / \  | / \ ||  /   _____   / \  | / \||  \      
+  | |  | ||  \__ \____\  | |  | |-| ||  \__ \____\  | |  | \_/||  /__     
+  \_/  \_/\____/         \_/  \_/ \_/\____/         \_/  \____/\____/
+""")
 
 game_mode = """
 Game mode available
@@ -23,11 +15,44 @@ Game mode available
     ━━━━━━━━━━━━━━━━━━━
     2. Player vs AI
     ━━━━━━━━━━━━━━━━━━━
+    3. Game History
+    ━━━━━━━━━━━━━━━━━━━
+    4. Quit
+    ━━━━━━━━━━━━━━━━━━━
+"""
+
+game_continue = """
+Game mode available
+
+    1. Home
+    ━━━━━━━━━━━━━━━━━━━
+    2. Quit
+    ━━━━━━━━━━━━━━━━━━━
+"""
+
+instructions = """
+This is the tic tac toe board
+
+   1 ┃ 2 ┃ 3
+ ━━━━╋━━━╋━━━━
+   4 ┃ 5 ┃ 6
+ ━━━━╋━━━╋━━━━
+   7 ┃ 8 ┃ 9
+
+*instruction :
+1. Insert the spot number (1-9) to put your sign
+2. You must fill all 9 spots to get the result
+3. Player 1 will go first 
 """
 
 sign_dictionary = []
-for i in range(9):
-    sign_dictionary.append(' ')
+def load_sign_dictionary():
+    for element in range(9):
+        sign_dictionary.append(" ")
+
+
+def reload_sign_dictionary():
+    sign_dictionary.clear()
 
 
 def print_board():
@@ -60,7 +85,18 @@ def take_input(player_name, board, sign):
                   "Please enter a number between 1-9.")
 
 
-def calculate_result(player_name):
+def take_input_end_game():
+    print(game_continue)
+    mode = int(input(""))
+    match mode:
+        case 1:
+            reload_sign_dictionary()
+            main()
+        case 2:
+            quit("Thank you both for joining.\n")
+
+
+def win_condition(player_name):
     if sign_dictionary[0] == sign_dictionary[1] == sign_dictionary[2] != ' ' or \
             sign_dictionary[0] == sign_dictionary[3] == sign_dictionary[6] != ' ' or \
             sign_dictionary[0] == sign_dictionary[4] == sign_dictionary[8] != ' ' or \
@@ -70,57 +106,64 @@ def calculate_result(player_name):
             sign_dictionary[3] == sign_dictionary[4] == sign_dictionary[5] != ' ' or \
             sign_dictionary[6] == sign_dictionary[7] == sign_dictionary[8] != ' ':
         print(f"\nCongratulation {player_name} !!\nYou WON !")
-        quit("Thank you both for joining.\n")
+        take_input_end_game()
 
 
 def main():
-    print("Welcome to the tic tac toe game !")
-    print(game_mode)
-    mode = int(input("Choose your game mode : "))
-    match mode:
-        case 1:
-            player_one = input("Enter player one name : ")
-            player_two = input("Enter player two name : ")
+    while True:
+        try:
+            print(game_mode)
+            mode = int(input("Choose your game mode : "))
+            match mode:
+                case 1:
+                    player_one = input("Enter player one name : ")
+                    player_two = input("Enter player two name : ")
 
-            print(f"Thank you for joining {player_one} and {player_two}")
-            print(instructions)
-            print(f"{player_one} is sign will be - X")
-            print(f"{player_two} is sign will be - O")
-            input("Press enter to start the game.")
+                    print(f"Thank you for joining {player_one} and {player_two}")
+                    print(f"{instructions}\n{player_one} is sign will be - X")
+                    print(f"{player_two} is sign will be - O")
+                    input("Press enter to start the game.")
 
-            print_board()
+                    load_sign_dictionary()
+                    print_board()
 
-            for j in range(9):
-                if j % 2 == 0:
-                    print(f"{player_one}, it's your turn")
-                    take_input(player_one, sign_dictionary, 'X')
-                    calculate_result(player_one)
-                else:
-                    print(f"{player_two}, it's your turn")
-                    take_input(player_one, sign_dictionary, 'O')
-                    calculate_result(player_two)
-                print_board()
-            print("This a TIE, Nobody Won. Play Again.")
+                    for i in range(9):
+                        if i % 2 == 0:
+                            print(f"{player_one}, it's your turn")
+                            take_input(player_one, sign_dictionary, 'X')
+                            win_condition(player_one)
+                        else:
+                            print(f"{player_two}, it's your turn")
+                            take_input(player_one, sign_dictionary, 'O')
+                            win_condition(player_two)
+                        print_board()
+                    print("This a TIE, Nobody Won. Play Again.")
 
-        case 2:
-            player_one = input("Enter player name : ")
+                case 2:
+                    player_one = input("Enter player name : ")
 
-            print(instructions)
-            print(f"{player_one} is sign will be - X")
-            input("Press enter to start the game.")
+                    print(f"{instructions}\n{player_one} is sign will be - X")
+                    input("Press enter to start the game.")
 
-            print_board()
+                    load_sign_dictionary()
+                    print_board()
 
-            for j in range(9):
-                if j % 2 == 0:
-                    print(f"{player_one}, it's your turn")
-                    take_input(player_one, sign_dictionary, 'X')
-                    calculate_result(player_one)
-                else:
-                    ia(sign_dictionary, 'O')
-                    calculate_result("AI")
-                print_board()
-            print("This a TIE, Nobody Won. Play Again.")
+                    for i in range(9):
+                        if i % 2 == 0:
+                            print(f"{player_one}, it's your turn")
+                            take_input(player_one, sign_dictionary, 'X')
+                            win_condition(player_one)
+                        else:
+                            ia(sign_dictionary, 'O')
+                            win_condition("AI")
+                        print_board()
+                    print("This a TIE, Nobody Won. Play Again.")
+
+                case 4:
+                    quit(":'(")
+
+        except ValueError:
+            continue
 
 
 main()
